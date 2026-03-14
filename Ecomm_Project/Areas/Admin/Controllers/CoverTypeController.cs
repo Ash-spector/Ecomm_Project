3425aeac7333051202476bbedf8df3968268fc0d
Ash-spector <shuklaaniket090@gmail.com>
2026-03-14 21:50:54 +0530

    Add controller for covertype

---
+++ Ecomm_Project/Areas/Admin/Controllers/CoverTypeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Ecomm_Project.Areas.Admin.Controllers
+{
+    public class CoverTypeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
