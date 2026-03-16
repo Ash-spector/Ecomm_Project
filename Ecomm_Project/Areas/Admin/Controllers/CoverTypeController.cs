3a4201e392a03f5f06882c3a3a7492406567ccc4
Ash-spector <shuklaaniket090@gmail.com>
2026-03-16 17:10:17 +0530

    Add Cover type

--- Ecomm_Project/Areas/Admin/Controllers/CoverTypeController.cs
+++ Ecomm_Project/Areas/Admin/Controllers/CoverTypeController.cs
@@ -1,12 +1,41 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Ecomm_Project.DataAccess.Repository.IRepository;
+using Ecomm_Project.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Ecomm_Project.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class CoverTypeController : Controller
     {
+        private readonly IUnitOfWork _unitOfWork;
+        public CoverTypeController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
         public IActionResult Index()
         {
             return View();
         }
+        public IActionResult Upsert(int? id)
+        {
+            CoverType coverType = new CoverType();
+
+            if (id == null || id == 0)
+                return View(coverType);
+
+            coverType = _unitOfWork.CoverType.Get(id.GetValueOrDefault());
+
+            if (coverType == null)
+                return NotFound();
+
+            return View(coverType);
+        }
+        #region APIs
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Json(new { data = _unitOfWork.CoverType.GetAll() });
+        }
+        #endregion
     }
 }
---
+++ Ecomm_Project/wwwroot/js/CoverType.js
@@ -0,0 +1,38 @@
+﻿var datatable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+function loadDataTable() {
+
+    datatable = $('#tblData').DataTable({
+        "ajax": {
+            "url": "/Admin/CoverType/GetAll"
+        },
+        "columns": [
+            { "data": "name", "width": "70%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `
+                    <div class="text-center">
+                        <a href="/Admin/CoverType/Upsert/${data}" class="btn btn-info">
+                            <i class="fas fa-edit"></i>
+                        </a>
+                        <a class="btn btn-danger" onclick="Delete('/Admin/CoverType/Delete/${data}')">
+                            <i class="fas fa-trash-alt"></i>
+                        </a>
+
+                    </div>
+                    `;
+                },
+                "width": "30%"
+            }
+        ]
+
+    });
+
+}
+function Delete(url) {
+    alert(url);
+}
